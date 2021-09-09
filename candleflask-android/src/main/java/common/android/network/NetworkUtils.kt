119c24fc9ea26e5b53c59da5a81581ffd21b1669
Amirul Zin <amirul.mdzin@gmail.com>
2021-09-10 01:01:44 +0800

    Remove unnecessary applicationContext in NetworkUtils

--- candleflask-android/src/main/java/common/android/network/NetworkUtils.kt
+++ candleflask-android/src/main/java/common/android/network/NetworkUtils.kt
@@ -12,7 +12,7 @@ import androidx.annotation.RequiresPermission
 @RequiresPermission(Manifest.permission.ACCESS_NETWORK_STATE)
 @TargetApi(Build.VERSION_CODES.M)
 fun Application.isNetworkConnected(): Boolean {
-  (applicationContext.getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager)?.let { cm ->
+  (getSystemService(Context.CONNECTIVITY_SERVICE) as? ConnectivityManager)?.let { cm ->
     val activeNetwork = cm.activeNetwork ?: return false
     return cm.getNetworkCapabilities(activeNetwork)
       ?.hasCapability(NetworkCapabilities.NET_CAPABILITY_VALIDATED)
